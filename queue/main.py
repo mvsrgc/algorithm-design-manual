083f7c80ba66809f3a9aa992da178e2f9320763d
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2021-09-02 09:08:20 -0400

    Queue.

--- queue/main.py
+++ queue/main.py
@@ -13,3 +13,28 @@ class Queue:
 
     def size(self):
         return len(self.items)
+
+    def __iter__(self):
+        self.n = 0
+        return self
+
+    def __next__(self):
+        if self.n < len(self.items):
+            result = self.items[self.n]
+            self.n += 1
+            return result
+        else:
+            raise StopIteration
+
+
+if __name__ == "__main__":
+    my_queue = Queue()
+
+    my_queue.enqueue(1)
+    my_queue.enqueue(2)
+    my_queue.enqueue(3)
+
+    my_queue.dequeue()
+
+    for item in my_queue:
+        print(item)
