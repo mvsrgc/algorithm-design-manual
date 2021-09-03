b85d7f425b3c9c447170dce0cdd27440b86808a0
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2021-09-03 08:38:06 -0400

    HashMap.

---
+++ hashmap/main.py
@@ -0,0 +1,82 @@
+class HashTable:
+    def __init__(self, size):
+        self.size = size
+        self.hash_table = self.create_buckets()
+
+    def create_buckets(self):
+        return [[] for _ in range(self.size)]
+
+    def set_val(self, key, val):
+        hashed_key = hash(key) % self.size
+
+        bucket = self.hash_table[hashed_key]
+
+        found_key = False
+        for index, record in enumerate(bucket):
+            record_key, record_val = record
+
+            if record_key == key:
+                found_key = True
+                break
+
+        if found_key:
+            bucket[index] = (key, val)
+        else:
+            bucket.append((key, val))
+
+    # Return searched value with specific key
+    def get_val(self, key):
+
+        # Get the index from the key using
+        # hash function
+        hashed_key = hash(key) % self.size
+
+        # Get the bucket corresponding to index
+        bucket = self.hash_table[hashed_key]
+
+        found_key = False
+        for index, record in enumerate(bucket):
+            record_key, record_val = record
+
+            # check if the bucket has same key as
+            # the key being searched
+            if record_key == key:
+                found_key = True
+                break
+
+        # If the bucket has same key as the key being searched,
+        # Return the value found
+        # Otherwise indicate there was no record found
+        if found_key:
+            return record_val
+        else:
+            return "No record found"
+
+    def delete_val(self, key, val):
+        hashed_key = hash(key) % self.size
+
+        bucket = self.hash_table[hashed_key]
+
+        found_key = False
+        for index, record in enumerate(bucket):
+            record_key, record_val = record
+
+            if record_key == key:
+                found_key = True
+                break
+        if found_key:
+            bucket.pop(index)
+
+        return
+
+    def __str__(self):
+        return "".join(str(item) for item in self.hash_table)
+
+
+if __name__ == "__main__":
+    hash_table = HashTable(50)
+
+    hash_table.set_val("ABC1234", "Joe")
+    hash_table.set_val("ABC1235", "Bob")
+
+    print(hash_table.get_val("ABC1235"))
