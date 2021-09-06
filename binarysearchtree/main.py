f59bbfd33d2b830e44ea5b7ed769d88a665821d8
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2021-09-06 19:10:07 -0400

    Delete.

--- binarysearchtree/main.py
+++ binarysearchtree/main.py
@@ -27,11 +27,11 @@ class BinarySearchTree:
         if self.root:
             return self._find(data, self.root)
         else:
-            return False
+            return None
 
     def _find(self, data, cur_node):
         if data == cur_node.data:
-            return True
+            return cur_node
 
         if data < cur_node.data and cur_node.left:
             return self._find(data, cur_node.left)
@@ -47,6 +47,43 @@ class BinarySearchTree:
         else:
             return None
 
+    def delete(self, data):
+        parent = None
+        node = self.root
+
+        while node and node.data != data:
+            parent = node
+            if data < node.data:
+                node = node.left
+            elif data > node.data:
+                node = node.right
+
+        # node not found
+        if node is None or node.data != data:
+            return None
+
+        # node has no children
+        elif node.left is None and node.right is None:
+            if node.data < parent.data:
+                parent.left = None
+            elif node.data > parent.data:
+                parent.right = None
+
+        # node has left child
+        elif node.left and node.right is None:
+            if node.data < parent.data:
+                parent.left = node.left
+            else:
+                parent.right = node.right
+
+        # node has right child
+        elif node.right and node.left is None:
+            if node.data < parent.data:
+                parent.left = node.right
+            else:
+                parent.right = node.right
+
+        # node has left and right child
 
 class Node:
     def __init__(self, data):
@@ -63,8 +100,10 @@ if __name__ == "__main__":
     tree.insert(4)
     tree.insert(45)
 
-    print(tree.find(12))
+    print(tree.find(12).data)
     print(tree.find(1))
     print(tree.find(13))
+
+    tree.delete(21)
 
     print(tree.find_minimum())
