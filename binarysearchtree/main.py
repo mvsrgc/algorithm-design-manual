1e9a2b998481d1f633573042ab19621ab379f85f
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2021-09-06 17:53:57 -0400

    Binary search tree search.

--- binarysearchtree/main.py
+++ binarysearchtree/main.py
@@ -23,6 +23,20 @@ class BinarySearchTree:
         else:
             print("Value is already present in tree.")
 
+    def find(self, data):
+        if self.root:
+            return self._find(data, self.root)
+        else:
+            return False
+
+    def _find(self, data, cur_node):
+        if data == cur_node.data:
+            return True
+
+        if data < cur_node.data and cur_node.left:
+            return self._find(data, cur_node.left)
+        elif data > cur_node.data and cur_node.right:
+            return self._find(data, cur_node.right)
 
 
 class Node:
@@ -31,5 +45,13 @@ class Node:
         self.left = None
         self.right = None
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
+    tree = BinarySearchTree(22)
+    tree.insert(12)
+    tree.insert(1)
+    tree.insert(45)
+
+    print(tree.find(12))
+    print(tree.find(1))
+    print(tree.find(13))
