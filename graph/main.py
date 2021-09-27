ce23caf9342a84796f8d2dc227ba5ca75df01fd7
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2021-09-27 12:54:46 -0400

    Graph.

--- graph/main.py
+++ graph/main.py
@@ -7,7 +7,7 @@ class Vertex:
         self.connected_to[neighbor] = weight
 
     def __str__(self):
-        return str(self.id) + " connectedTo: " + str([x.id for x in self.connectedTo])
+        return str(self.id) + " connectedTo: " + str([x.id for x in self.connected_to])
 
     def connections(self):
         return self.connected_to.keys()
@@ -29,3 +29,26 @@ class Graph:
         new_vertex = Vertex(key)
         self.vertices[key] = new_vertex
         return new_vertex
+
+    def vertex(self, n):
+        if n in self.vertices:
+            return self.vertices[n]
+        else:
+            return None
+
+    def __contains__(self, n):
+        return n in self.vertices
+
+    def add_edge(self, f, t, weight=0):
+        if f not in self.vertices:
+            nv = self.add_vertex(f)
+        if t not in self.vertices:
+            nv = self.add_vertex(t)
+
+        self.vertices[f].add_neighbor(self.vertices[t], weight)
+
+    def vertices(self):
+        return self.vertices.keys()
+
+    def __iter__(self):
+        return iter(self.vertices.values())
